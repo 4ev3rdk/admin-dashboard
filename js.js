77b55929a8d153b93f02f57e68da5986bb270d52
Dev Suthar <4ev3rdk@gmail.com>
2026-07-17 23:05:22 +0530

    Added Dark and Light themes and gave CSS finishing like hoverig etc

--- js.js
+++ js.js
@@ -4,3 +4,17 @@ title_img.addEventListener("click", () => { document.querySelector("sidebar").cl
 if (window.innerWidth <= 480) {
     document.querySelector("sidebar").classList.add("collapsed");
 }
+
+//change the theme
+const lightBtn = document.getElementById("light_btn");
+const darkBtn = document.getElementById("dark_btn");
+
+lightBtn.addEventListener("click", () => {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add("light");
+});
+
+darkBtn.addEventListener("click", () => {
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.add("dark");
+});
